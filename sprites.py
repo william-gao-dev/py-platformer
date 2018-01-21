47fd0fb9f1d41fa51dc79090d8311323c94f0acc
william-gao-dev <35654586+william-gao-dev@users.noreply.github.com>
2018-01-22 00:17:38 +1100

    Added platform class

--- sprites.py
+++ sprites.py
@@ -11,19 +11,35 @@ class Player(pg.sprite.Sprite):
         self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
         self.rect.center = (WIDTH/2,HEIGHT/2)
-## Velocity
         self.pos = vec(WIDTH/2,HEIGHT/2)
         self.vel = vec(0,0)
         self.acc = vec(0,0)
     def update(self):
-        self.acc = vec(0,0)
+        self.acc = vec(0,0.5)
         keys = pg.key.get_pressed()
         if keys[pg.K_LEFT]:
             self.acc.x = -PLAYER_ACC
         if keys[pg.K_RIGHT]:
             self.acc.x = PLAYER_ACC
-        self.acc += self.vel * PLAYER_FRICTION
+
+        # applys friction
+        self.acc.x += self.vel.x * PLAYER_FRICTION # takes x values of tuple self.acc
+        # equations of motion
         self.vel += self.acc
         self.pos += self.vel + 0.5 * self.acc
+        # wrap around the sides of the screen
+        print (self.pos.x)
+        if self.pos.x > WIDTH:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = WIDTH
         self.rect.center = self.pos
-##
+
+class Platform(py.sprite.Sprite):
+    def __init__(self,x,y,w,h):
+        py.sprite.Sprite.__init__(self)
+        self.image = pg.Surface((w,h))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
